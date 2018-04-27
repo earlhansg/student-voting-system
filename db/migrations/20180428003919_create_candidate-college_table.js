9e95c2d1b67459b502dfc56ee19d34942e2932e4
earlhansg <earlhansg@gmail.com>
2018-04-28 01:16:05 +0800

    #33 Create Schema for Candidates

---
+++ db/migrations/20180428003919_create_candidate-college_table.js
@@ -0,0 +1,14 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('candidate-college', function(table) {
+    table.increments();
+    table.integer('student_id').references('id_number').inTable('student');
+    table.integer('position_id').references('id').inTable('position');
+    table.integer('election_id').references('id').inTable('election-college');
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+    table.timestamp('updated_at').defaultTo(knex.fn.now())
+  })
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('candidate-college');
+}
