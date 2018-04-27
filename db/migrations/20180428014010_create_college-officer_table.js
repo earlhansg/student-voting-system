a86dbb9843f1a57790d1bbf90a99834ef5189ccb
earlhansg <earlhansg@gmail.com>
2018-04-28 01:50:09 +0800

    #27 Create Schema for Elected Officers

---
+++ db/migrations/20180428014010_create_college-officer_table.js
@@ -0,0 +1,14 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('college-officer', function(table) {
+    table.increments();
+    table.integer('type').references('id').inTable('election-college');
+    table.integer('position_id').references('id').inTable('position');
+    table.integer('student_id').references('id_number').inTable('student');
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+    table.timestamp('updated_at').defaultTo(knex.fn.now())
+  })
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('college-officer');
+}
